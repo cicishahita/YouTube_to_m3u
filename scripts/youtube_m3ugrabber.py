19aeec298f5a3341cedbe52bd100a08f1e0d7156
cicishahita <86391109+cicishahita@users.noreply.github.com>
2022-06-21 07:29:54 +0700

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -8,17 +8,32 @@ banner = r'''
 #     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
 #     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
 #                   |__/                                                #
-#                                                                       #
+#                                  >> https://github.com/benmoose39     #
 #########################################################################
 '''
 
 import requests
+import os
+import sys
+
+windows = False
+if 'win' in sys.platform:
+    windows = True
 
 def grab(url):
-    response = requests.get(url).text
+    response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
-        print('https://raw.githubusercontent.com/cicishahita/YouTube_to_m3u/main/assets/moose_na.m3u')
-        return
+        #response = requests.get(url).text
+        if '.m3u8' not in response:
+            if windows:
+                print('https://raw.githubusercontent.com/cicishahita/YouTube_to_m3u/main/assets/moose_na.m3u')
+                return
+            #os.system(f'wget {url} -O temp.txt')
+            os.system(f'curl "{url}" > temp.txt')
+            response = ''.join(open('temp.txt').readlines())
+            if '.m3u8' not in response:
+                print('https://raw.githubusercontent.com/cicishahita/YouTube_to_m3u/main/assets/moose_na.m3u')
+                return
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
@@ -33,6 +48,7 @@ def grab(url):
 
 print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
 print(banner)
+#s = requests.Session()
 with open('../youtube_channel_info.txt') as f:
     for line in f:
         line = line.strip()
@@ -47,4 +63,7 @@ with open('../youtube_channel_info.txt') as f:
             print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
             grab(line)
-
+            
+if 'temp.txt' in os.listdir():
+    os.system('rm temp.txt')
+    os.system('rm watch*')
